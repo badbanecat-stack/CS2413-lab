410c561d96a74b1a33592592ca6e80b88d93d61d
badbanecat-stack <140128238+badbanecat-stack@users.noreply.github.com>
2025-05-07 15:13:46 -0500

    Create lab_4_problem_1.c

---
+++ lab_4_problem_1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+bool isMirror(struct TreeNode* t1, struct TreeNode* t2) {
+    if (!t1 && !t2) return true;
+    if (!t1 || !t2) return false;
+    return (t1->val == t2->val) &&
+           isMirror(t1->left, t2->right) &&
+           isMirror(t1->right, t2->left);
+}
+
+bool isSymmetric(struct TreeNode* root) {
+    if (!root) return true;
+    return isMirror(root->left, root->right);
+}
+
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+int main() {
+    struct TreeNode* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(2);
+    root->left->left = newNode(3);
+    root->left->right = newNode(4);
+    root->right->left = newNode(4);
+    root->right->right = newNode(3);
+
+    if (isSymmetric(root))
+        printf("Tree is symmetric\n");
+    else
+        printf("Tree is not symmetric\n");
+
+    return 0;
+}
